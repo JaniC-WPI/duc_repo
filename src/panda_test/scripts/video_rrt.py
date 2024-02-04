39eec1fbeba70388e061def2189ca93d99c94e53
JaniC-WPI <schatterjee@wpi.edu>
2024-02-04 16:19:23 -0500

    video scripts update

--- src/panda_test/scripts/video_rrt.py
+++ src/panda_test/scripts/video_rrt.py
@@ -2,13 +2,13 @@ import cv2
 import os
 
 # Parameters
-image_folder = '/home/jc-merlab/Pictures/panda_data/images_for_occlusion/1/'
-video_name = '/home/jc-merlab/Pictures/panda_data/images_for_occlusion/1/occ_test_1.avi'
+image_folder = '/home/jc-merlab/Pictures/panda_data/images_for_occlusion/4/'
+video_name = '/home/jc-merlab/Pictures/panda_data/images_for_occlusion/4/occ_test_4.avi'
 frame_rate = 10  # Frames per second
 extra_frames_for_last_image = 3  # Number of extra times to add the last image
 
 images = []
-for i in range(830,1003):
+for i in range(109):
     image_path = os.path.join(image_folder, f'image_{i}.jpg')
     if os.path.exists(image_path):
         images.append(image_path)
